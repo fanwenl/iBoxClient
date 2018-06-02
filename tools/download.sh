ea314036203a2c533b20c96fb1c6503a1bb8d0ef
fanwenlong <fanwenl_g@126.com>
2018-06-02 21:35:38 +0800

    add download script for linux

---
+++ tools/download.sh
@@ -0,0 +1,19 @@
+#/bin/bash
+echo $0
+echo $1
+if [ -e $1 ];
+then
+echo $1 "文件存在"
+if [ -e $PWD/jlink.jlink ];
+then
+rm $PWD/jlink.jlink
+echo "删除已有的jlink.jlink"
+fi
+echo "h" >> jlink.jlink
+echo "loadbin" $1 "0x08000000" >> jlink.jlink
+echo "g" >> jlink.jlink
+echo "qc" >> jlink.jlink
+JLinkExe -device STM32F103ZE -Speed 4000 -IF JTAG -JTAGConf -1,-1 -CommanderScript jlink.jlink
+else
+echo $1 "文件不存在"
+fi
---
+++ tools/readme.md
@@ -0,0 +1,8 @@
+1.download 为linux 程序下载脚本.
+    使用方法为：
+    ` ./download.sh filename `
+    filename为需要下载的文件(.hex)
+2.在linux下vs code 可以下载和调试该工程，需要按照Cortex—Debug插件。
+3.keilkill.bat windows下的keil工程文件清理脚本。
+    使用方法：
+    将keilkill.bat 复制到工程的根目录中运行(在tools目录中执行没有弄好)。
